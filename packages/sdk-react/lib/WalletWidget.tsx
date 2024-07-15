a03aedf1499f7ae78fca49d89de51a5a5706d5cc
Simon Šenk <simon.senk@kalmia.si>
2024-07-15 13:53:04 +0200

    prevent double react createRoot

--- packages/sdk-react/lib/WalletWidget.tsx
+++ packages/sdk-react/lib/WalletWidget.tsx
@@ -1,10 +1,21 @@
+import { useEffect, useRef } from 'react';
 import { initializeApp, AppProps } from '@oasis-app-wallet/sdk/ui';
 import '@oasis-app-wallet/sdk/css';
 
 export function WalletWidget({ className, ...params }: AppProps & { className?: string }) {
-  setTimeout(() => {
-    initializeApp('#oasis-app-wallet-activator-react', params);
-  }, 5);
+  const initialized = useRef(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      if (initialized.current) {
+        return;
+      }
+
+      initialized.current = true;
+
+      initializeApp('#oasis-app-wallet-activator-react', params);
+    }, 5);
+  }, []);
 
   return <div id="oasis-app-wallet-activator-react" className={className}></div>;
 }
