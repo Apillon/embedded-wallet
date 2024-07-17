d72a739c4063bb08ea00b3b332d189a70f1eea99
Simon Šenk <simon.senk@kalmia.si>
2024-07-17 15:50:40 +0200

    mv test api

--- apps/apillon-api/server.js
+++ apps/apillon-api/server.js
@@ -8,14 +8,14 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
-app.use(express.json())
+app.use(express.json());
 
 app.get('/session-token', async (req, res) => {
   const response = await apillonAuthAPI.get('/session-token');
   res.json(response.data);
-})
+});
 
 const port = 3000;
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
-})
+});
