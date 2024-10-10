18eedc658bfffbc84951874351dd403347a56c79
Simon Šenk <simon.senk@kalmia.si>
2024-10-10 16:45:09 +0200

    multi rpc wip

--- packages/sdk/lib/utils.ts
+++ packages/sdk/lib/utils.ts
@@ -60,3 +60,54 @@ export function abort(e: keyof typeof Errors, message = 'Error') {
   err.name = Errors[e];
   throw err;
 }
+
+export class RetriableJsonRpcProvider extends ethers.JsonRpcProvider {
+  providerList: ethers.JsonRpcProvider[];
+  currentIndex = 0;
+  error: any;
+
+  constructor(rpcs: string[], chainId: number) {
+    super(rpcs[0], chainId);
+
+    this.providerList = rpcs.map(url => new ethers.JsonRpcProvider(url, chainId));
+  }
+
+  async send(method: string, params: Array<any>, retry = 0): Promise<any> {
+    // Throw if all providers checked
+    if (retry === this.providerList.length) {
+      const error = this.error;
+      this.error = undefined;
+      throw new Error(error);
+    }
+
+    try {
+      const provider = this.selectProvider();
+
+      /**
+       * @TODO set timeout to like 15s, default is 300s
+       * @TODO add another retry cycle
+       * @url https://github.com/ethers-io/ethers.js/issues/4122
+       */
+      return await provider.send(method, params);
+    } catch (error) {
+      // store error internally
+      this.error = error;
+
+      retry = retry + 1;
+
+      return this.send(method, params, retry);
+    }
+  }
+
+  private selectProvider() {
+    if (this.currentIndex === this.providerList.length) {
+      this.currentIndex = 1;
+      return this.providerList[0];
+    }
+
+    const provider = this.providerList[this.currentIndex];
+    this.currentIndex = this.currentIndex + 1;
+
+    return provider;
+  }
+}
