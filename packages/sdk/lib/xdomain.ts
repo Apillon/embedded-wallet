084bb229b95a4e6aae8e57840999b3b6b56ce53b
Simon Šenk <simon.senk@kalmia.si>
2024-11-26 16:56:53 +0100

    safari popup workaround; load/close handling;

--- packages/sdk/lib/xdomain.ts
+++ packages/sdk/lib/xdomain.ts
@@ -9,9 +9,10 @@ export class XdomainPasskey {
   src = import.meta.env.VITE_XDOMAIN_PASSKEY_SRC ?? 'https://passkey.apillon.io';
   popup: WindowProxy | null = null;
   loadPromise: { resolve: () => void; reject: (e: any) => void } | undefined;
-  lastEventId = 0; // use this to match iframe response with promise resolvers
-  promises: { id: number; resolve: (v: any) => void; reject: (e: any) => void }[] = [];
+  isPopupLoaded = false;
   popupCheckInterval: null | ReturnType<typeof setInterval> = null; // monitor if popup was closed
+  promises: { id: number; resolve: (v: any) => void; reject: (e: any) => void }[] = [];
+  lastEventId = 0; // use this to match iframe response with promise resolvers
 
   constructor() {
     window.addEventListener('message', this.onResponse.bind(this));
@@ -40,9 +41,16 @@ export class XdomainPasskey {
         this.popup.close();
         this.popup = null;
       }
-    } else if (ev?.data?.type === 'apillon_pk_load' && this.loadPromise) {
-      this.loadPromise.resolve();
+
+      this.isPopupLoaded = true;
       this.loadPromise = undefined;
+    } else if (ev?.data?.type === 'apillon_pk_load') {
+      this.isPopupLoaded = true;
+
+      if (this.loadPromise) {
+        this.loadPromise.resolve();
+        this.loadPromise = undefined;
+      }
     }
   }
 
@@ -62,21 +70,25 @@ export class XdomainPasskey {
       this.popupCheckInterval = null;
     }
 
-    const width = 400;
-    const height = 400;
+    // Safari workaround
+    // `window.open` has to be called in non-async context.
+    setTimeout(() => {
+      const width = 400;
+      const height = 400;
 
-    this.popup = window.open(
-      this.src,
-      '_blank',
-      [
-        `width=${width}`,
-        `height=${height}`,
-        `left=${Math.round(window.innerWidth / 2 + window.screenX - width / 2)}`,
-        `top=${Math.round(window.innerHeight / 2 + window.screenY - height / 2)}`,
-        `location=no`,
-        `resizable=no`,
-      ].join(',')
-    );
+      this.popup = window.open(
+        this.src,
+        '_blank',
+        [
+          `width=${width}`,
+          `height=${height}`,
+          `left=${Math.round(window.innerWidth / 2 + window.screenX - width / 2)}`,
+          `top=${Math.round(window.innerHeight / 2 + window.screenY - height / 2)}`,
+          `location=no`,
+          `resizable=no`,
+        ].join(',')
+      );
+    }, 1);
 
     // Keep checking if popup window was closed
     this.popupCheckInterval = setInterval(() => {
@@ -99,14 +111,21 @@ export class XdomainPasskey {
 
         this.popup = null;
         this.loadPromise = undefined;
+        this.isPopupLoaded = false;
       } else if (this.popup?.closed === undefined && this.popupCheckInterval) {
         clearInterval(this.popupCheckInterval);
         this.popupCheckInterval = null;
+        this.loadPromise = undefined;
+        this.isPopupLoaded = false;
       }
     }, 500);
 
     // Wait for popup window content to load (resolves on `apillon_pk_load` postMessage event)
     await new Promise<void>((resolve, reject) => {
+      if (this.isPopupLoaded) {
+        return resolve();
+      }
+
       this.loadPromise = { resolve, reject };
     });
   }
